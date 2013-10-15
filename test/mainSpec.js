6ac627ef00ca500998b3a52296603b43b4b645f9
elf Pavlik <perpetual-tripper@wwelves.org>
2013-10-15 13:31:22 +0200

    bootstrapped with tests karma + mocha + chai + sinon

---
+++ test/mainSpec.js
@@ -0,0 +1,22 @@
+describe('Main', function(){
+
+  describe('config', function(){
+
+    it('has *config*');
+  
+  });
+
+  describe('operator', function(){
+
+    it('creates local operator');
+
+  });
+
+  describe('profile', function(){
+  });
+
+  describe('world', function(){});
+
+  describe('map', function(){});
+
+});
